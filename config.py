11e3fde5bc9691514c270963e83ee63ef33b67b2
DeadBoter <dhananjaythakur264@gmail.com>
2025-01-03 22:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link
    # ON_HEROKU = True
 #else:
    # ON_HEROKU = False
-#URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
