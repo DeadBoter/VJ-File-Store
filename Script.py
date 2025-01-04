8b01b50c57f728e0071851a713f27e5649529ea4
DeadBoter <dhananjaythakur264@gmail.com>
2025-01-04 19:49:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@
 
 
 class script(object):
-    START_TXT = """ʜᴇʟʟᴏ, {} ɪ'ᴍ {} 👙, ʜᴇʀᴇ ᴛᴏ ғᴜʟʟғɪʟʟ ʏᴏᴜʀ ᴘᴏᴍ ᴘᴏᴍ ᴅᴇsɪʀᴇ 🥵"""
+    START_TXT = """<b>ʜᴇʟʟᴏ {}, ɪ ᴀᴍ {} ɪ ᴄᴀɴ ғᴜʟʟғɪʟʟ ʏᴏᴜʀ ᴘᴏᴍ ᴘᴏᴍ ᴅᴇsɪʀᴇ 👙</b>"""
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
