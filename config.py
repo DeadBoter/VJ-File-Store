470d4d29a7652721415337cb70e36682e8cac45b
DeadBoter <dhananjaythakur264@gmail.com>
2025-01-05 16:29:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,98 +1,7 @@
-# # Don't Remove Credit Tg - @VJ_Botz
-# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# # Ask Doubt on telegram @KingVJ01
-
-
-# import re
-# import os
-# from os import environ
-# from Script import script
-
-# id_pattern = re.compile(r'^.\d+$')
-# def is_enabled(value, default):
-#     if value.lower() in ["true", "yes", "1", "enable", "y"]:
-#         return True
-#     elif value.lower() in ["false", "no", "0", "disable", "n"]:
-#         return False
-#     else:
-#         return default
-      
-# # Bot Information
-# API_ID = int(environ.get("API_ID", "22787995"))
-# API_HASH = environ.get("API_HASH", "6801f07baf8559043bee01d19550ad8b")
-# BOT_TOKEN = environ.get("BOT_TOKEN", "")
-
-# PICS = (environ.get('PICS', 'https://iili.io/2Ux7U3x.md.jpg')).split() # Bot Start Picture
-# ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7218217238').split()]
-# BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
-# PORT = environ.get("PORT", "8080")
-
-# # Clone Info :-
-# CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
-
-# # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-# CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-# CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
-
-# # Database Information
-# DB_URI = environ.get("DB_URI", "")
-# DB_NAME = environ.get("DB_NAME", "techvjbotz")
-
-# # Auto Delete Information
-# AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
-
-# # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-# AUTO_DELETE = int(environ.get("AUTO_DELETE", "15")) # Time in Minutes
-# AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "900")) # Time in Seconds
-
-# # Channel Information
-# LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002260692728"))
-
-# # File Caption Information
-# CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-# BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-
-# # Enable - True or Disable - False
-# PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
-
-# # Verify Info :-
-# VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
-
-# # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-# SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-# SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
-# VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-
-# # Website Info:
-# WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
-
-# # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-# WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
-
-# # File Stream Config
-# STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
-
-# # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-# MULTI_CLIENT = False
-# SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-# PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-# if 'DYNO' in environ:
-#     ON_HEROKU = True
-# else:
-#     ON_HEROKU = False
-# URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
-
-
-# # Don't Remove Credit Tg - @VJ_Botz
-# # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# # Ask Doubt on telegram @KingVJ01
-    
-# ..............................................................................................................................................................
-
-
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 import re
 import os
@@ -100,7 +9,6 @@ from os import environ
 from Script import script
 
 id_pattern = re.compile(r'^.\d+$')
-
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -108,31 +16,37 @@ def is_enabled(value, default):
         return False
     else:
         return default
-
+      
 # Bot Information
 API_ID = int(environ.get("API_ID", "22787995"))
 API_HASH = environ.get("API_HASH", "6801f07baf8559043bee01d19550ad8b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://iili.io/2Ux7U3x.md.jpg')).split()  # Bot Start Picture
+PICS = (environ.get('PICS', 'https://iili.io/2Ux7U3x.md.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7218217238').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "")  # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
 PORT = environ.get("PORT", "8080")
 
-# Channel Information
-try:
-    LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002260692728"))
-except ValueError:
-    LOG_CHANNEL = None  # Handle invalid channel ID
+# Clone Info :-
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+
+# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True))  # Set True or False
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "15"))  # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "900"))  # Time in Seconds
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+
+# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "15")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "900")) # Time in Seconds
+
+# Channel Information
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002260692728"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -141,25 +55,35 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 # Enable - True or Disable - False
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
 
-# Logging Test (Improved Logging)
-def log_message_test(bot):
-    try:
-        if LOG_CHANNEL:
-            bot.send_message(LOG_CHANNEL, "Logging Test: Bot has started successfully!")
-            print("Log message sent to LOG_CHANNEL successfully!")
-        else:
-            print("LOG_CHANNEL is not set or invalid.")
-    except Exception as e:
-        print(f"Error while sending log message: {e}")
+# Verify Info :-
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
- if 'DYNO' in environ:
+# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+
+# Website Info:
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+
+# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+
+# File Stream Config
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+
+# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
-
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+    
